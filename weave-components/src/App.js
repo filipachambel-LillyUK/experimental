18e3d1bd1881a40a5dc2f433abdce38e067d3674
filipachambel-LillyUK <filipa.chambel@network.lilly.com>
2024-10-29 13:25:49 +0000

    changes in chart

--- weave-components/src/App.js
+++ weave-components/src/App.js
@@ -12,28 +12,44 @@ import routes from './routes';
 import Alerts from './Alert/Alerts';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.jsx';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const year = 2016;
+  const [data, setData] = useState([]);
+  const year = json.year;
+  const endYearOffset = 3;
   const date = +echarts.time.parse(year + '-01-01');
-  const end = +echarts.time.parse(+year + 1 + '-01-01');
+  const end = +echarts.time.parse(+year + endYearOffset + '-01-01');
   const dayTime = 3600 * 24 * 1000;
-  const data = [];
+
   const options = json.option;
-  for (let time = date; time < end; time += dayTime) {
-    data.push([
-      echarts.time.format(time, '{yyyy}-{MM}-{dd}', false),
-      Math.floor(Math.random() * 10000)
-    ]);
-  }
+
+  // for (let time = date; time < end; time += dayTime) {
+  //   data.push([
+  //     echarts.time.format(time, '{yyyy}-{MM}-{dd}', false),
+  //     Math.floor(Math.random() * 10000)
+  //   ]);
+  // }
+
+  useEffect(() => {
+    const generatedData = [];
+    for (let time = date; time < end; time += dayTime) {
+      generatedData.push([
+        echarts.time.format(time, "{yyyy}-{MM}-{dd}", false),
+        Math.floor(Math.random() * 10000),
+      ]);
+    }
+    setData(generatedData);
+  }, [date, end, dayTime]);
+
   return (
     <Provider store={store}>
     <div className="App" data-testid='app-component'>
    
     <Router>
-         <Breadcrumbs paths={routes}/>
-         <Alerts title="New title" content="New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content" position="topRight" type="warning"/>
-      {/* <Chart options={options} data={data}/> */}
+         {/* <Breadcrumbs paths={routes}/> */}
+         {/* <Alerts title="New title" content="New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content New content" position="topRight" type="warning"/> */}
+      <Chart options={options}/>
       <Routes>
       {routes.map((route, index) => (
         <Route key={index} path={route.path} element={route.component}/>
